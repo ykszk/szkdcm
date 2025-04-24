076864dc1a28072a6748864b00ac63f39cf8f097
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2025-04-24 17:07:55 +0900

    Fix tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
-use std::fs;
 use anyhow::Result;
+use std::fs;
+use std::path::PathBuf;
 use szkdcm::Args;
 
 #[test]
@@ -9,20 +9,27 @@ fn test_dump() -> Result<()> {
         let path = dicom_test_files::path(format!("pydicom/{name}.dcm").as_str()).unwrap();
         println!("DICOM file: {:?}", path);
         let output = PathBuf::from(env!("CARGO_TARGET_TMPDIR")).join(format!("{name}.csv"));
-        println!("Output file: {:?}", output);    
+        println!("Output file: {:?}", output);
         let args = Args {
             input: vec![path],
-            tag: ["PatientName", "PatientID", "PixelSpacing", "ImagerPixelSpacing"].iter()
-                .map(|s| s.parse().unwrap())
-                .collect(),
+            tag: [
+                "PatientName",
+                "PatientID",
+                "PixelSpacing",
+                "ImagerPixelSpacing",
+            ]
+            .iter()
+            .map(|s| s.parse().unwrap())
+            .collect(),
             read_until: "PixelData".to_string(),
             tag_file: vec![],
             output: Some(output.clone()),
             jobs: None,
+            complete: None,
         };
         let result = szkdcm::main(args);
         result.unwrap();
-        
+
         // Snapshot the output CSV content
         let content = fs::read_to_string(output)?;
         insta::assert_snapshot!(format!("{name}_output"), content);
@@ -36,24 +43,31 @@ fn test_multiple_files() -> Result<()> {
         dicom_test_files::path("pydicom/liver.dcm").unwrap(),
         dicom_test_files::path("pydicom/CT_small.dcm").unwrap(),
     ];
-    
+
     let output = PathBuf::from(env!("CARGO_TARGET_TMPDIR")).join("multiple_files.csv");
-    
+
     let args = Args {
         input: paths,
-        tag: ["Modality", "PatientID", "PixelSpacing", "ImagerPixelSpacing"].iter()
-            .map(|s| s.parse().unwrap())
-            .collect(),
+        tag: [
+            "Modality",
+            "PatientID",
+            "PixelSpacing",
+            "ImagerPixelSpacing",
+        ]
+        .iter()
+        .map(|s| s.parse().unwrap())
+        .collect(),
         read_until: "PixelData".to_string(),
         tag_file: vec![],
         output: Some(output.clone()),
         jobs: Some(2),
+        complete: None,
     };
-    
+
     szkdcm::main(args)?;
-    
+
     let content = fs::read_to_string(output)?;
     insta::assert_snapshot!("multiple_files_output", content);
-    
+
     Ok(())
 }
